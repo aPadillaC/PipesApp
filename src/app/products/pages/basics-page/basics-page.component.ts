3e67787a178fdaea31656c5a9faeda492ab6fc31
Antonio Padilla <antoniopc88@gmail.com>
2023-06-06 11:35:50 +0200

    first commit

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrls: ['./basics-page.component.css']
+})
+export class BasicsPageComponent {
+
+  public nameLower: string = 'antonio';
+  public nameUpper: string = 'ANTONIO';
+  public fullName: string = 'aNtOnIo PaDiLlA';
+
+  public customDate: Date = new Date();
+}
